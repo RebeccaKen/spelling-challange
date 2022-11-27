cf84eaa94f670d21c7305e149bd31c2cd8a1b430
RebeccaKen <kennedyrebecca694@gmail.com>
2022-11-27 21:41:20 +0000

    Add hangman images, intialize varibles, constants and create function to generate random word

--- run.py
+++ run.py
@@ -1,3 +1,77 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+# Constants
+
+hangman_images = ['''
+    +---+    
+    |   |
+        |
+        |
+        |
+        |
+        |
+===========''', '''
+    +---+    
+    |   |
+    O   |
+        |
+        |
+        |
+        |
+===========''', '''
+    +---+    
+    |   |
+    O   |
+    |   |
+        |
+        |
+        |
+===========''', '''
+    +---+    
+    |   |
+    O   |
+   /|   |
+        |
+        |
+        |
+===========''', '''
+    +---+    
+    |   |
+    O   |
+   /|\  |
+        |
+        |
+        |
+===========''', '''
+    +---+    
+    |   |
+    O   |
+   /|\  |
+   /    |
+        |
+        |
+===========''', '''
+    +---+    
+    |   |
+    O   |
+   /|\  |
+   / \  |
+        |
+        |
+===========''']
+
+words = ['ENJOY', 'HONEY', 'HAPLESS', 'GROAN', 'STATEMENT', 'TEENY', 'PHYSICAL', 'WRETCHED', 'TURKEY', 'TRAINS', 'FLY', 'STRAPS']
+
+plays_remaining = 11
+
+guessed_letters = '-'
+
+wrong_guesses = 0
+
+used_letters = []
+
+# Function to generate a random word
+
+def pick_a_word():
+    word_position = random.randint(0, len(words) - 1)
+    return words[word_position]
