0adfb8d1e5aade6ab439c77f065f85dec6260bc7
RebeccaKen <kennedyrebecca694@gmail.com>
2022-11-29 02:36:10 +0000

    Add more options to greeting function

--- run.py
+++ run.py
@@ -60,29 +60,27 @@ hangman_images = ['''
         |
 ===========''']
 
-words = ['ENJOY', 'HONEY', 'HAPLESS', 'GROAN', 'STATEMENT', 'TEENY', 'PHYSICAL', 'WRETCHED', 'TURKEY', 'TRAINS', 'FLY', 'STRAPS']
-
-plays_remaining = 11
-
-
-# Function to generate a random word
-
 def pick_a_word():
-    word_position = random.randint(0, len(words) - 1)
-    return words[word_position]
+    """
+    This function will generate a word that the user will try to guess
+    
+    """
+    words = ['ENJOY', 'HONEY', 'HAPLESS', 'GROAN', 'STATEMENT', 'TEENY', 'PHYSICAL', 'WRETCHED', 'TURKEY', 'TRAINS', 'FLY', 'STRAPS']
+    return random.choice(words).upper()
 
-    answer = pick_a_word()
+# Welcome messages
+name = input ('Welcome to Hangman. Please enter your player name:' )
+if name.isalpha():
+        print("Hello " + name + " it's nice to meet you" + "!")
+        print('Welcome to Hangman. Ready to swing?')
+        guess = input('Guess a letter: ')
+elif name.isdigit():
+        print('Sorry, you can only to use letters only to spell your player name!')
+else:
+    print('You cannot use any special characters in player name.')
 
-# Welcome message
-
-print('Welcome to Hangman. Ready to swing?')
-
-guess = input('Guess a letter: ').upper()
-
-"""
-This function will construct a display board for the user
-"""
-def displayBoard(missedLetters, correctLetters, answer):
+ 
+def displayBoard(missedLetters, correctLetters, words):
     print(hangman_images[len(missedLetters)])
     print()
 
@@ -92,28 +90,18 @@ def displayBoard(missedLetters, correctLetters, answer):
         print(letter, end=' ')
     print()
 
-    blanks = '_' * len(answer)
+    blanks = '_' * len(words)
 
     # Replace blanks with correctly guessed letters.
 
-    for i in range(len(answer)): 
-        if answer[i] in correctLetters:
-            blanks = blanks[:i] + answer[i] + blanks[i+1:]
+    for i in range(len(words)): 
+        if words[i] in correctLetters:
+            blanks = blanks[:i] + words[i] + blanks[i+1:]
             print('Right!')
 
     # Show the answer with spaces in between each letter.
 
     for letter in blanks: 
         print(letter, end=' ')
-print()
+        print()
 
-
-def getGuess(alreadyGuessed):
-    # Returns the letter the player entered. This function makes sure the player entered a single letter and not something else.
-        while True:
-             guess = input()
-             guess = guess.upper()
-             if len(guess) != 1:
-                 print('Please enter a single letter.')
-                 elif guess in alreadyGuessed:
-                     print('You already entered that letter. Pick another letter')
