e416cd567a76a7d3ec8c259911ce1be8f18b17ae
RebeccaKen <kennedyrebecca694@gmail.com>
2022-12-04 22:06:17 +0000

    Add main function to restart game

--- run.py
+++ run.py
@@ -1,93 +1,104 @@
-import random
+def main():
 
-# Code to connect run.py to word list in list.txt for use in secretWord
-with open("list.txt", "r") as new_list:
-    words = [item.replace("\n", "") for item in new_list.readlines()]
+    import random
 
-secretWord = random.choice(words)
+    # Code to connect run.py to word list in list.txt for use in secretWord
+    with open("list.txt", "r") as new_list:
+        words = [item.replace("\n", "") for item in new_list.readlines()]
 
-"""
-This try/except statement will collect the player's age.
-It then validates their input.
-If the player is not between 8-11 a message
-will appear to clarify the recommended player age.
-"""
+    secretWord = random.choice(words)
 
-while True:
-    try:
-        num = int(input("Please enter your age:\n"))
-    except ValueError:
-        print("Please enter a valid number")
-        continue
-    if num >= 8 and num <= 11:
-        print(f'You entered: {num}')
-        break
-    else:
-        print("This game is recommended for children age 8 - 11!")
+    def get_Name():
+        '''This welcome Function collects player's name as an input.'''
+        response = input("Welcome to Spelling Challange!Please enter your name:\n")
+        return response
 
+    name = get_Name()
 
-def get_Name():
-    '''This welcome Function collects player's name as an input.'''
-    response = input("Welcome to Spelling Challange!Please enter your name:\n")
-    return response
+    """
+    This try/except statement will collect the player's age.
+    It then validates their input.
+    If the player is not between 8-11 a message
+    will appear to clarify the recommended player age.
+    """
 
-name = get_Name()
-
-# Input validation method will be used to assure the player's name uses letters
-if name.isalpha():
-    print(f"Hello {name} it's nice to meet you" + "!")
-    print("Ready to go?")
-elif name.isdigit():
-    print("Sorry, you can only to use letters only to spell your player name!")
-else:
-    print("You cannot use any special characters in player name.")
-
-
-correct = []
-incorrect = []
-fail_Count = 8
-letters_Guessed = " "
-game_Done = True
-
-
-# The following code was created using the tutorial
-# 'How to Build a Hangman Game with Python' by CBT Nuggets.
-
-def play_Again():
-    """Function returns True if the player wants to play again."""
-    print("Do you want to play Spelling Challange again? (yes or no)")
-    return input().lower().startswith('y')
-
-
-while fail_Count > 0:
-
-    print("===============================")
-
-    guess = input("Please enter a letter:")
-
-    if guess in secretWord:
-        correct.append(guess)
-        print(f"Correct! There is one or more {guess} in the secretWord")
-    else:
-        fail_Count -= 1
-        incorrect.append(guess)
-        print(f"Incorrect! There is no {guess} in the answer.")
-
-    letters_Guessed = letters_Guessed + guess
-    fail_Count = 0
-
-    for letter in secretWord:
-        if letter in letters_Guessed:
-            print(f"{letter}", end=" ")
+    while True:
+        try:
+            num = int(input("Please enter your age:\n"))
+        except ValueError:
+            print("Please enter a valid number")
+            continue
+        if num >= 8 and num <= 11:
+            print(f'You entered: {num}')
+            break
         else:
-            print("_", end=" ")
-            fail_Count += 1
+            print("This game is recommended for children age 8 - 11!")
+
+
+    # Input validation method will be used to assure the player's name uses letters
+    if name.isalpha():
+        print(f"Hello {name} it's nice to meet you" + "!")
+        print("Ready to go?")
+    elif name.isdigit():
+        print("Sorry, you can only to use letters only to spell your player name!")
+    else:
+        print("You cannot use any special characters in player name.")
+
+
+    correct = []
+    incorrect = []
+    fail_Count = 8
+    letters_Guessed = " "
+    game_Done = True
+
+
+    # The following code was created using the tutorial
+    # 'How to Build a Hangman Game with Python' by CBT Nuggets.
+
+    while fail_Count > 0:
+
+        print("===============================")
+
+        guess = input("Please enter a letter:")
+
+        if guess in secretWord:
+            correct.append(guess)
+            print(f"Correct! There is one or more {guess} in the answer")
+        else:
+            fail_Count -= 1
+            incorrect.append(guess)
+            print(f"Incorrect! There is no {guess} in the answer.")
+
+        letters_Guessed = letters_Guessed + guess
+        fail_Count = 0
+
+        for letter in secretWord:
+            if letter in letters_Guessed:
+                print(f"{letter}", end=" ")
+            else:
+                print("_", end=" ")
+                fail_Count += 1
 
             # Check if player's guesses are all correct.
-        if len(correct) == len(secretWord):
-            print("You won!")
-            break
-        # Check if player has guessed too many times and lost.
-        if len(incorrect) == len(secretWord) - 1:
-            print(f"You've run out of guesses. Answer = {secretWord}")
-            break
+            if len(correct) == len(secretWord):
+                print(f"You won! The answer was {secretWord}")
+                game_Done = True
+                break
+            # Check if player has guessed too many times and lost.
+            if len(incorrect) == len(secretWord) - 1:
+                print(f"You've run out of guesses. Answer = {secretWord}")
+                game_Done = True
+                break
+
+    restart = input("Start again? y for Yes, n for NO: ")
+    if restart == ("y"):
+        incorrect = []
+        incorrect = []
+        main()
+    else:
+        quit()
+
+
+main()
+
+
