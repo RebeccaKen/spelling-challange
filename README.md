2c6044a6af6c9176e7cff31017fed2405867ab28
RebeccaKen <kennedyrebecca694@gmail.com>
2022-12-05 07:44:12 +0000

    Try and solve issue with loop failure

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
  <!DOCTYPE html>
 <html>
 <head>
-<title>README.MD</title>
+<title></title>
 </head>
 <body>
 
--- run.py
+++ run.py
@@ -15,52 +15,40 @@ def main():
 
     name = get_Name()
 
-    """
-    This try/except statement will collect the player's age.
-    It then validates their input.
-    If the player is not between 8-11 a message
-    will appear to clarify the recommended player age.
-    """
-
-    while True:
-        try:
-            num = int(input("Please enter your age:\n"))
-        except ValueError:
-            print("Please enter a valid number")
-            continue
-        if num >= 8 and num <= 11:
-            print(f'You entered: {num}')
-            break
-        else:
-            print("This game is recommended for children age 8 - 11!")
-
     # validation method will be used to assure the player's name uses letters
     if name.isalpha():
         print(f"Hello {name} it's nice to meet you" + "!")
         print("Ready to go?")
     elif name.isdigit():
-        print("Sorry, you can only to use letters!")
+        print("Please use letters!")
     else:
         print("You cannot use any special characters in player name.")
+        
+    # This try/except statement will collect the player's age.
+    # It then validates their input.
+    # If the player is not between 8-11 a message
+    # will appear to clarify the recommended player age.
 
     correct = []
     incorrect = []
-    fail_Count = 7
-    letters_Guessed = " "
     game_Done = True
+    fail_Count = 0
+    letterGuessed = ' '
+    secretWord = random.choice(words)
+    guess = ' '
 
     # The following while loop code was created using the tutorial
     # 'How to Build a Hangman Game with Python' by CBT Nuggets.
-
+    
     while fail_Count > 0:
 
         print("===============================")
 
-        guess = input("Please enter a letter:")
+        guess = input("Please enter a letter:\n")
 
         if guess in secretWord:
             correct.append(guess)
-            print(f"Correct! There is one or more {guess} in the answer")
+            print(f"Correct! There is a {guess}")
             fail_Count -= 1
         else:
             incorrect.append(guess)
@@ -76,27 +64,26 @@ def main():
                 print("_", end=" ")
                 fail_Count += 1
 
-            # Check if player's guesses are all correct.
+        # Check if player's guesses are all correct.
             if len(correct) == len(secretWord):
                 print(f"You won! The answer was {secretWord}")
                 print("=====================================")
                 game_Done = True
                 break
-            # Check if player has guessed too many times and lost.
-            if len(incorrect) == len(secretWord) - 1:
+            else:
                 print(f"You've run out of guesses. Answer = {secretWord}")
                 print("===================================================")
                 game_Done = True
                 break
 
-    # Restart game if user chooses to do so using main()
-    restart = input("Start again? y for Yes, n for NO:\n")
-    if restart == ("y"):
-        incorrect = []
-        incorrect = []
-        main()
-    else:
-        quit()
 
+        # Restart game if user chooses to do so using main()
+        restart = input("Start again? y for Yes, n for No:\n")
+        if restart == ("y"):
+           incorrect = []
+           correct = []
+           main()
+        else:
+            quit()
 
 main()
